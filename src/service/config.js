e2b3ce432651d1cbac8330dcf9bdf6f971e79710
RenatoShishido <renato.re2012@hotmail.com>
2020-03-23 01:48:33 -0400

    arrumando bagunca do frontend

--- src/service/config.js
+++ src/service/config.js
@@ -2,9 +2,10 @@ import axios from 'axios'
 
 const token = `Bearer ${localStorage.getItem('jwt')}`
 
+console.log(token)
 
-export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+export const http = axios.create({
+  baseURL: 'http://localhost:3000/',
   transformRequest: [function (data, headers) {
     headers['Authorization'] = token
     return JSON.stringify(data)
@@ -13,5 +14,3 @@ export const api = axios.create({
     'Content-Type': 'application/json'
   }
 })
-
-
