6cf449a151b86670739668facaf4b6bd86aacfa9
RenatoShishido <renato.re2012@hotmail.com>
2020-03-26 22:16:26 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-var express = require('express');
-var serveStatic = require('serve-static');
-var app = express();
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path')
 
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use('/', serveStatic(path.join(__dirname + "/dist")));
+
+
+const port = process.env.PORT || 8080;
 app.listen(port);
+
 console.log('server started '+ port);
 
   
