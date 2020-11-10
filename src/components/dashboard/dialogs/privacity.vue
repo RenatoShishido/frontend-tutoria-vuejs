f231163f3b01694738627178cf7199551d8d2fee
DiegoBulhoes <diegobulhoesm@gmail.com>
2020-11-10 17:15:32 -0400

    Fix name component - privacity

--- src/components/dashboard/dialogs/privacity.vue
+++ src/components/dashboard/dialogs/privacity.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="text-center">
-    <v-dialog
-      v-model="dialog"
-      persistent
-      width="500"
-    >
+    <v-dialog v-model="dialog" persistent width="500">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          class="white--text"
-          v-bind="attrs"
-          v-on="on"
-        >
+        <v-btn color="primary" class="white--text" v-bind="attrs" v-on="on">
           Cadastrar
         </v-btn>
       </template>
@@ -22,27 +13,23 @@
         </v-card-title>
 
         <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </v-card-text>
 
         <v-divider></v-divider>
 
         <v-card-actions>
           <v-spacer></v-spacer>
-           <v-btn
-            color="green darken-1"
-            text
-            @click="dialog = false"
-          >
-            Disagree
+          <v-btn color="green darken-1" text @click="dialog = false">
+            Não aceito
           </v-btn>
-          <v-btn
-            color="primary"
-            text
-            @click="cadastrar()"
-          >
-            Eu aceito
-          </v-btn>
+          <v-btn color="primary" text @click="cadastrar()"> Eu aceito </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -50,21 +37,20 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        dialog: false,
-      }
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  methods: {
+    cadastrar() {
+      this.$emit("cadastrarUsuario");
+      this.dialog = false;
     },
-    methods: {
-        cadastrar() {
-            this.$emit('cadastrarUsuario')
-            this.dialog = false
-        }
-    }
-  }
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/views/dashboard/View-Perfil.vue
+++ src/views/dashboard/View-Perfil.vue
@@ -17,7 +17,7 @@
         </v-col>
         <v-col>
           <h1>Tutorias Oferecidas</h1>
-          <TableTutoria :data="offerTutoria" /> 
+          <TableTutoria :data="offerTutoria" />
         </v-col>
       </v-row>
     </v-container>
@@ -29,6 +29,7 @@ import serviceTutoria from "../../service/tutorias";
 import DashPerfil from "../../components/dashboard/perfilDashboard";
 import TableTutoria from "../../components/table";
 import RatingValue from "../../components/rating";
+
 export default {
   name: "View-Perfil",
   components: {
@@ -52,34 +53,33 @@ export default {
       const response = await serviceTutoria.findTutoriaAll();
       const params = location.pathname.split("/");
 
-
       response.tutorias.forEach((element) => {
-        if(element.tutor !== undefined) {
-          if(element.tutor._id === params[3]) {
-            this.offerTutoria.push(element)
+        if (element.tutor !== undefined) {
+          if (element.tutor._id === params[3]) {
+            this.offerTutoria.push(element);
           }
         }
       });
 
       response.tutorias.forEach((element) => {
-      if(element.status !== "AguardandoAluno")
-        if (element.user._id === params[3]) {
-          this.myTutoria.push(element);
-        }
+        if (element.status !== "AguardandoAluno")
+          if (element.user._id === params[3]) {
+            this.myTutoria.push(element);
+          }
       });
 
       this.RateValue = this.myTutoria.length + this.offerTutoria / 5;
     },
   },
-   watch: {
+  watch: {
     async $route(to, from) {
       if (to.params.id) {
         if (to.params.id != from.params.id) {
-          await document.location.reload()
+          await document.location.reload();
         }
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
